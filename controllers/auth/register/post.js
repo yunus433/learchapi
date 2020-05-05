e707d7138512892c19acf9d704db08d43c6d36fd
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2020-05-05 22:53:24 +0300

    Add /auth/register controller

---
+++ controllers/auth/register/post.js
@@ -0,0 +1,27 @@
+const validator = require('validator');
+
+const User = require('../../../models/user/User');
+
+module.exports = (req, res, next) => {
+  if (!req.body.email || !req.body.password)
+    return res.status(400).json({ error: "bad request" });
+
+  if (!validator.isEmail(req.body.email))
+    return res.status(400).json({ error: "not valid email" });
+
+  const newUserData = {
+    email: req.body.email,
+    password: req.body.password
+  }; 
+
+  const newUser = new User(newUserData);
+
+  newUser.save((err, user) => {
+    if (err && err.code == 11000)
+      return res.status(400).json({ error: "already taken email" });
+
+    if (err || !user) return res.status(500).json({ error: "mongo error: " + err });
+
+    return res.status(200).json({ user });
+  });
+}
